a932110ac37704b65d9c9ee421260b18353b5e17
Nata Makarova <81148530+iymtee@users.noreply.github.com>
2023-04-12 19:53:11 +0300

    Create proc MakeFamilyPurchase

---
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -0,0 +1,8 @@
+create procedure MakeFamilyPurchase (@FamilySurName varchar(255))
+as
+if ((select count(SurName) from Family where Family.SurName like @FamilySurName) > 0)
+update Family
+set dbo.Family.BudgetValue = dbo.Family.BudgetValue - (select sum([Value]) from dbo.Basket where Family.SurName = @FamilySurName)
+where Family.SurName = @FamilySurName
+else
+raiserror('Семьи с заданной фамилией - не существует!', 16, 1)
