94413a3da7b0c5b4e7e79653ed00c9bccaf42840
iymtee <umari666ru@gmail.com>
2023-04-23 15:07:22 +0300

    Update dbo.usp_MakeFamilyPurchase.sql
    Изменение оформления. Вопрос: set и if кидать ли select'ы на новые строки?

--- dbo.usp_MakeFamilyPurchase.sql
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -1,8 +1,10 @@
-create procedure MakeFamilyPurchase (@FamilySurName varchar(255))
+create or alter procedure MakeFamilyPurchase (@FamilySurName varchar(255)) 
 as
 if ((select count(SurName) from Family where Family.SurName like @FamilySurName) > 0)
-update Family
-set dbo.Family.BudgetValue = dbo.Family.BudgetValue - (select sum([Value]) from dbo.Basket where Family.SurName = @FamilySurName)
-where Family.SurName = @FamilySurName
+begin
+	update Family
+	set dbo.Family.BudgetValue = dbo.Family.BudgetValue - (select sum([Value]) from dbo.Basket where Family.SurName = @FamilySurName)
+	where Family.SurName = @FamilySurName
+end
 else
-raiserror('Семьи с заданной фамилией - не существует!', 16, 1)
+	raiserror('Семьи с заданной фамилией - не существует!', 16, 1)
