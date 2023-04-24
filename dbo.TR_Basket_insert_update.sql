0ba033af1fabd3f05ddcfe2d151293afb53bccd3
iymtee <umari666ru@gmail.com>
2023-04-24 18:07:29 +0300

    Update dbo.TR_Basket_insert_update.sql
    Изменения в оформлении

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -3,10 +3,16 @@ on  dbo.Basket
 for insert, update
 as
 if ((select top(1) ID_SKU from dbo.Basket  where id <> (select id from inserted) order by id desc) = (select ID_SKU from inserted))
-update dbo.Basket
-set DiscountValue = (select [Value] from dbo.Basket where (select id from inserted) = id ) * 0.5
-where id = (select id from inserted) and id = (select top(1) ID_SKU from dbo.Basket order by id)
+begin
+	update dbo.Basket
+	set DiscountValue = (select [Value] from dbo.Basket where (select id from inserted) = id ) * 0.5
+	where id = (select id from inserted) 
+		  and id = (select top(1) ID_SKU from dbo.Basket order by id)
+end
 else 
-update dbo.Basket
-set DiscountValue = 0
-where  id = (select id from inserted) and id = (select top(1) ID_SKU from dbo.Basket order by id)
+begin
+	update dbo.Basket
+	set DiscountValue = 0
+	where id = (select id from inserted) 
+		  and id = (select top(1) ID_SKU from dbo.Basket order by id)
+end
