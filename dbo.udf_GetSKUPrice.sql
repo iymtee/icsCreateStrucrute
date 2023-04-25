24dfac6e6d85600e10f4ada2e0d1bc58d22a06d6
iymtee <umari666ru@gmail.com>
2023-04-25 17:40:55 +0300

    Update dbo.udf_GetSKUPrice.sql
    Изменения оформления 2.0

--- dbo.udf_GetSKUPrice.sql
+++ dbo.udf_GetSKUPrice.sql
@@ -1,10 +1,9 @@
 create or alter function GetSKUPrice (@ID_SKU as int)
-	returns decimal(18, 2)
-	begin 
-		declare @price decimal(18,2)
-		set @price = (select sum([Value])/sum(Quantity)
-						from dbo.Basket
-					   where @ID_SKU = Basket.ID_SKU)
-		return @price
-	end
-go
+returns decimal(18, 2)
+begin 
+	declare @price decimal(18,2)
+	set @price = (select sum([Value])/sum(Quantity)
+				  from dbo.Basket
+				  where @ID_SKU = Basket.ID_SKU)
+	return @price
+end
