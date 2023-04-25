12d07a26e8f09f87539e5272622544b6d2f340d8
iymtee <umari666ru@gmail.com>
2023-04-25 17:38:45 +0300

    Update dbo.TR_Basket_insert_update.sql
    Изменения оформления 2.0

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -2,17 +2,31 @@ create or alter trigger TR_Basket_insert_update
 on  dbo.Basket
 for insert, update
 as
-if ((select top(1) ID_SKU from dbo.Basket  where id <> (select id from inserted) order by id desc) = (select ID_SKU from inserted))
+if ((select top(1) ID_SKU 
+	 from dbo.Basket  
+	 where id <> (select id 
+				  from inserted) 
+				  order by id desc) = (select ID_SKU 
+									   from inserted))
 begin
 	update dbo.Basket
-	set DiscountValue = (select [Value] from dbo.Basket where (select id from inserted) = id ) * 0.5
-	where id = (select id from inserted) 
-		  and id = (select top(1) ID_SKU from dbo.Basket order by id)
+	set DiscountValue = (select [Value] 
+						 from dbo.Basket 
+						 where (select id 
+								from inserted) = id ) * 0.5
+	where id = (select id 
+				from inserted) 
+		  and id = (select top(1) ID_SKU 
+					from dbo.Basket 
+					order by id)
 end
 else 
 begin
 	update dbo.Basket
 	set DiscountValue = 0
-	where id = (select id from inserted) 
-		  and id = (select top(1) ID_SKU from dbo.Basket order by id)
+	where id = (select id 
+				from inserted) 
+		  and id = (select top(1) ID_SKU 
+					from dbo.Basket 
+					order by id)
 end
