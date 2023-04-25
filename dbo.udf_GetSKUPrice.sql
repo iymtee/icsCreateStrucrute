f37ad495e0a5a0aa292b6d16d7efe6a5259b1d25
Nata Makarova <81148530+iymtee@users.noreply.github.com>
2023-04-25 17:50:40 +0300

    Update dbo.udf_GetSKUPrice.sql
    Изменение отображения на гите

--- dbo.udf_GetSKUPrice.sql
+++ dbo.udf_GetSKUPrice.sql
@@ -3,7 +3,7 @@ returns decimal(18, 2)
 begin 
 	declare @price decimal(18,2)
 	set @price = (select sum([Value])/sum(Quantity)
-				  from dbo.Basket
-				  where @ID_SKU = Basket.ID_SKU)
+		      from dbo.Basket
+		      where @ID_SKU = Basket.ID_SKU)
 	return @price
 end
