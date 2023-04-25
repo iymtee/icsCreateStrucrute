30113c162414b05f43655d23b42a43ec2b09aa2d
Nata Makarova <81148530+iymtee@users.noreply.github.com>
2023-04-25 17:53:01 +0300

    Update dbo.usp_MakeFamilyPurchase.sql
    Изменение отображения на гите

--- dbo.usp_MakeFamilyPurchase.sql
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -1,14 +1,14 @@
 create or alter procedure MakeFamilyPurchase (@FamilySurName varchar(255)) 
 as
 if ((select count(SurName) 
-	from Family 
-	where Family.SurName like @FamilySurName) > 0)
+     from Family 
+     where Family.SurName like @FamilySurName) > 0)
 begin
 	update Family
 	set dbo.Family.BudgetValue = dbo.Family.BudgetValue - (select sum([Value]) 
-														   from dbo.Basket 
-														   where Family.SurName = @FamilySurName)
+								from dbo.Basket 
+								where Family.SurName = @FamilySurName)
 	where Family.SurName = @FamilySurName
 end
 else
-	raiserror('Семьи с заданной фамилией - не существует!', 16, 1)
+	raiserror('РЎРµРјСЊРё СЃ Р·Р°РґР°РЅРЅРѕР№ С„Р°РјРёР»РёРµР№ - РЅРµ СЃСѓС‰РµСЃС‚РІСѓРµС‚!', 16, 1)
