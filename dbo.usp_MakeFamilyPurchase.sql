b9daa0cce36f02909ebaea1453f60d0192f4557b
iymtee <umari666ru@gmail.com>
2023-04-25 17:44:48 +0300

    Update dbo.usp_MakeFamilyPurchase.sql
    Изменения оформления 2.0

--- dbo.usp_MakeFamilyPurchase.sql
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -1,9 +1,13 @@
 create or alter procedure MakeFamilyPurchase (@FamilySurName varchar(255)) 
 as
-if ((select count(SurName) from Family where Family.SurName like @FamilySurName) > 0)
+if ((select count(SurName) 
+	from Family 
+	where Family.SurName like @FamilySurName) > 0)
 begin
 	update Family
-	set dbo.Family.BudgetValue = dbo.Family.BudgetValue - (select sum([Value]) from dbo.Basket where Family.SurName = @FamilySurName)
+	set dbo.Family.BudgetValue = dbo.Family.BudgetValue - (select sum([Value]) 
+														   from dbo.Basket 
+														   where Family.SurName = @FamilySurName)
 	where Family.SurName = @FamilySurName
 end
 else
